f65019a6b2aeee8d4867b0b6fe52b370e1f7198a
semdinsp <scott.sproule@ficonab.com>
2020-07-06 21:20:26 +0800

    now has links to three main projects

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -21,6 +21,9 @@ export default () => (
           className="primary-content" 
           dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
         />
+        <Link to='https://recycle.verde-tl.com' className="button -primary">Recycle and Community Project &rarr;</Link>
+        <Link to='https://marketinng.verde-tl.com' className="button -primary">Marketing Project &rarr;</Link>
+        <Link to='https://app.verde-tl.com' className="button -primary">Marketinng App &rarr;</Link>
         <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
       </div>
     )}
