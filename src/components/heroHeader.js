1c38f07bdfb3823ca18a7c8d6aef9f826745cc63
semdinsp <scott.sproule@ficonab.com>
2020-11-11 12:28:22 +0800

    now with marekting 2 link

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -22,7 +22,7 @@ export default () => (
           dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
         />
         <a href='https://recycle.verde-tl.com' className="button -primary">Recycle and Community Project &rarr;</a>
-        <a href='https://marketing.verde-tl.com' className="button -primary">Marketing Project &rarr;</a>
+        <a href='https://marketing2.verde-tl.com' className="button -primary">Marketing Project &rarr;</a>
         <a href='https://app.verde-tl.com' className="button -primary">Marketing App &rarr;</a>
         <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
       </div>
