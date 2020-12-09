39c26ab5d642f236402f14c9c0761755f2efc384
semdinsp <scott.sproule@ficonab.com>
2020-12-09 18:33:13 +0800

    new version of analytics using gtab

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,13 +26,7 @@ export default ({ children }) => {
       {children}
       <footer className="site-footer">
         <p>&copy; {new Date().getFullYear()} Verdeka Lda &bull; Crafted with <span role="img" aria-label="love">❤️</span></p>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JLHZF6XKFH"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-JLHZF6XKFH');
-        </script>
+        
       </footer>
     </div>
   )
